118946b6dbdb8c8762c789edc1a36ee1e831a8f1
archivoccp@gmail.com <archivoccp@gmail.com>
2024-08-29 15:30:21 -0500

    se agregan componentes modificados al repositorio

---
+++ src/Paginas/NoticiaMercantil/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BotelinMercantil } from "../../Componentes/BoletinMercantil";
+
+const NoticiaMercantil = () => {
+    return(
+        <div>
+            <BotelinMercantil />
+        </div>
+    )
+};
+
+export {NoticiaMercantil};
